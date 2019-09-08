224c5216d68e559101ed9517bbc759be62241860
NikkiFord <nikkigford@gmail.com>
2019-09-07 19:14:09 -0600

    Make the site mobile friendly.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,12 +2,16 @@ $("#aboutLink").click((e) => {
   e.preventDefault();
   $(".page-content").hide();
   $("#aboutContent").show();
+  $(".active").removeClass("active");
+  $("#aboutLink").addClass("active");
 });
 
 $("#portfolioLink").click((e) => {
   e.preventDefault();
   $(".page-content").hide();
   $("#portfolioContent").show();
+  $(".active").removeClass("active");
+  $("#portfolioLink").addClass("active");
 });
 
 $("#aboutLink").click();
